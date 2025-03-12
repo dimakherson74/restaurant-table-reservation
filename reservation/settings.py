e89a8b88c592be6af72a3814ee155cbb33b48988
Dolgov Dmytro <dimakherson74@gmail.com>
2025-03-12 20:46:45 +0100

    add to settings.py CRISPY_ALLOWED_TEMPLATE_PACKS = bootstrap5 CRISPY_TEMPLATE_PACK = bootstrap5

--- reservation/settings.py
+++ reservation/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "reservation",
-    "restaurant_reservation"
+    "restaurant_reservation",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
