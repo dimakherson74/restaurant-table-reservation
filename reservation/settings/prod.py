1a89edc1ae8037301df69f0d276173dd834444d2
Dolgov Dmytro <dimakherson74@gmail.com>
2025-03-16 19:34:36 +0100

    Fix DisallowedHost error

--- reservation/settings/prod.py
+++ reservation/settings/prod.py
@@ -5,7 +5,11 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [
+    "restaurant-table-reservation-1.onrender.com",
+    "127.0.0.1",
+    "localhost"
+]
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
